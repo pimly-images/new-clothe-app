d8cd23860c29d28dbc2773ae38cfa987334c048a
miho-kubota <adg4618@gmail.com>
2026-05-15 01:19:35 +0900

    feat: 全てのコンフリクトを解消し、Aboutページへのリンクを完全実装

--- src/main.jsx
+++ src/main.jsx
@@ -1,29 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-<<<<<<< HEAD
-<<<<<<< HEAD
-import App from './App' // ここで App.tsx または App.jsx を読み込んでいます
-=======
-import { BrowserRouter } from 'react-router-dom' // これが必要です
-import App from './App.jsx'
->>>>>>> 13cc7f9 (feat: React Routerを導入し、Aboutページを追加)
-=======
-import App from './App' // ここで App.tsx または App.jsx を読み込んでいます
->>>>>>> 9d403b62e4b21fd59057021da30572dccfa19e4e
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
 import './index.css'
+import App from './App.jsx'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-<<<<<<< HEAD
-<<<<<<< HEAD
-    <App />
-=======
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
     <BrowserRouter>
       <App />
     </BrowserRouter>
->>>>>>> 13cc7f9 (feat: React Routerを導入し、Aboutページを追加)
-=======
-    <App />
->>>>>>> 9d403b62e4b21fd59057021da30572dccfa19e4e
-  </React.StrictMode>,
+  </StrictMode>,
 )
